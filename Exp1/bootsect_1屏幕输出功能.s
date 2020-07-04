d662fd135c933ca6f38e8ecd73708b81dcc47773
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-04 19:55:18 +0800

    Update and rename bootsect1.s to bootsect_1屏幕输出功能.s

--- Exp1/bootsect_1屏幕输出功能.s
+++ Exp1/bootsect_1屏幕输出功能.s
@@ -1,3 +1,4 @@
+！完成 bootsect.s 的屏幕输出功能
 ！核心代码
 
 entry _start
@@ -6,21 +7,22 @@ _start:
     xor bh,bh
     int 0x10
     
-    mov cx,#36          ! 要显示的字符串长度
+    mov cx,#26          ! 将显示的字符串长度改为26
     mov bx,#0x0007
     mov bp,#msg1
     
-    mov ax,#0x07c0
-    mov es,ax
+    mov ax,#0x07c0      ！这两行是自己添加进去的
+    mov es,ax           ! 相比与 linux-0.11 中的代码，需要增加对 es 的处理，因为原代码中在输出之前已经处理了 es
+    
     mov ax,#0x1301
     int 0x10
     
-inf_loop:               ! 设置一个无限循环
+inf_loop:               ! 设置一个无限循环，使得光标停在这里，不用继续执行下去
     jmp inf_loop
 
 msg1:                   ! msg1 处放置字符串 
     .byte   13,10
-    .ascii  "Hello OS world, my name is LZJ"
+    .ascii  "HongOS is Loading..."    ！利洪的洪
     .byte   13,10,13,10
 
 .org 510                 ! boot_flag 必须在最后两个字节,即510地址处
