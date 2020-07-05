e6fbe85512fa2422470fb0a75a70cdabb37c2b37
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-05 15:45:06 +0800

    Update 1屏幕输出功能_bootsect.s

--- Exp2/1屏幕输出功能_bootsect.s
+++ Exp2/1屏幕输出功能_bootsect.s
@@ -1,19 +1,23 @@
-！完成 bootsect.s 的屏幕输出功能
-！核心代码
+
+!核心代码
 
 entry _start
 _start:
+
+!打印HongOS is Loading...
     mov ah,#0x03       ! 首先读入光标位置
     xor bh,bh
     int 0x10
     
     mov cx,#26          ! 将显示的字符串长度改为26
     mov bx,#0x0007
-    mov bp,#msg1
-    
-    mov ax,#0x07c0      ！这两行是自己添加进去的
-    mov es,ax           ! 相比与 linux-0.11 中的代码，需要增加对 es 的处理，因为原代码中在输出之前已经处理了 es
-    
+    mov bp,#msg1        !标号为msg1
+ 
+ !---------这两行是自己添加进去的---------------------
+    mov ax,#0x07c0      !相比与 linux-0.11 中的代码，需要增加对 es 的处理
+    mov es,ax           !因为linux-0.11有把bootsect.c从07c0搬运到了9000位置
+ !-------------------------------------------------
+ 
     mov ax,#0x1301
     int 0x10
     
@@ -22,7 +26,7 @@ inf_loop:               ! 设置一个无限循环，使得光标停在这里，
 
 msg1:                   ! msg1 处放置字符串 
     .byte   13,10
-    .ascii  "HongOS is Loading..."    ！利洪的洪
+    .ascii  "HongOS is Loading..."    ！利洪的洪，字符长度为26
     .byte   13,10,13,10
 
 .org 510                 ! boot_flag 必须在最后两个字节,即510地址处
