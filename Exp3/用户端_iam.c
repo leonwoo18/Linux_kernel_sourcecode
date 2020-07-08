7126d7709c1673bee1e491199391e713515b5e90
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 03:55:08 +0800

    Create 用户端_iam.c

---
+++ Exp3/用户端_iam.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<inputname.h>
+int main(int argc, char *argv[])
+{
+    int r;
+    if(argc!=2)
+    {
+        puts("please input your name."); //提醒用户执行文件时，带上名字参数
+        r=-1;
+    }
+    else
+    {
+        iam(argv[1]);  //iam()会去调用封装在inputname.h中的接口函数
+        r=0;
+    }
+    return r;
+}
