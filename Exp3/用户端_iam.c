b8d7839b412792a20f274eefc3e16c46952a3891
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 05:59:21 +0800

    Update 用户端_iam.c

--- Exp3/用户端_iam.c
+++ Exp3/用户端_iam.c
@@ -3,15 +3,15 @@
 int main(int argc, char *argv[])
 {
     int r;
-    if(argc!=2)
+    if(argc<2)
     {
-        puts("please input your name."); //提醒用户执行文件时，带上名字参数
+        puts("please input your name.\n"); /*提醒用户执行文件时，带上名字参数*/
         r=-1;
     }
     else
     {
-        iam(argv[1]);  //iam()会去调用封装在inputname.h中的接口函数
-                       //argv[1]为用户输入的参数
+        iam(argv[1]);  /*iam()会去调用封装在inputname.h中的接口函数*/
+                       /*argv[1]为用户输入的参数*/
         r=0;
     }
     return r;
