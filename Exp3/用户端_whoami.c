3957ae1556f7e50cfe64a88d3f9936ae176a2b39
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 05:57:30 +0800

    Update 用户端_whoami.c

--- Exp3/用户端_whoami.c
+++ Exp3/用户端_whoami.c
@@ -1,11 +1,11 @@
-#include <usname.h>
+#include <inputname.h>
 #include <stdio.h>
 int main(void)
 {
-    char name[24];   //在用户态申请一个name数组空间
+    char name[24];   /*在用户态申请一个name数组空间*/
 	
-    whoami(name, 24);      //iam()会去调用封装在inputname.h中的接口函数
-	                   //传入用户态name数组地址，和size要求
+    whoami(name, 24);      /*iam()会去调用封装在inputname.h中的接口函数*/
+	                   /*传入用户态name数组地址，和size要求*/
     printf("%s",name);
     return 0;
 }
