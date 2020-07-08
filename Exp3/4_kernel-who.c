71aa786368956042ca0abccbc9c93b1acb34a855
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 03:30:26 +0800

    Update 4_kernel-who.c

--- Exp3/4_kernel-who.c
+++ Exp3/4_kernel-who.c
@@ -4,62 +4,45 @@
 #include <errno.h>
 #include <asm/segment.h>
 
-char msg[24]; //23个字符 +'\0' = 24
-
+char username[24]=""; //用来赋值从用户态拿到的数据
 int sys_iam(const char * name)
-/***
-function：将name的内容拷贝到msg,name的长度不超过23个字符
-return：拷贝的字符数。如果name的字符个数超过了23,则返回“­-1”,并置errno为EINVAL。
-****/
 {
-/***
-下面的printk（）供iam.c测试用
-printk("congratulations! system call successfully\n");
-****/
-	int i;
-	//临时存储 输入字符串 操作失败时不影响msg
-	char tmp[30];
-	for(i=0; i<30; i++)
+        //printk("congratulations! system call successfully\n");此句供测试用
+
+	int len=0;   //用于记录用户输入的字符串长度
+	char tmp[30];  //临时的内核态字符串数组，用来放从用户态拿到的数据
+	
+	//复制用户态内存的数组到内核态的临时数组
+	for(int i=0; i<30; i++)
 	{
-		//从用户态内存取得数据
-		tmp[i] = get_fs_byte(name+i);
-		if(tmp[i] == '\0') break;  //字符串结束
+	    tmp[i] = get_fs_byte(&name[i]); //关键的get_fs_byte(用户态的地址)，就能拿到用户态的数据了
+	    if(temp[i]!='\0')
+                len++;
+            else
+                break;   //字符串复制结束，并算出用户输入的长度len
 	}
-	//printk(tmp);
-	i=0;
-	while(i<30&&tmp[i]!='\0') i++;
-	int len = i;
-	// int len = strlen(tmp);
-	//字符长度大于23个
-	if(len > 23)
-	{
-		// printk("String too long!\n");
-		return -(EINVAL);  //置errno为EINVAL  返回“­-1”  具体见_syscalln宏展开
-	}
-	strcpy(msg,tmp);
-	//printk(tmp);
-	return i;
+	if(len<24)
+            strcpy(username, temp);
+        else
+	    int n=-EINVAL;
+	return n;     //-(EINVAL)即-22，存入到EAX寄存器
+	              //调用返回后，从 EAX 取出返回值，存入 __res，__res来自unistd.h中的_syscalln宏展开
+	              //即此时__res=-22,宏展开的代码通过对 __res 的判断决定传给 API 的调用者什么样的返回值
+	              //errno = -__res，即errno=22，调用者就可以通过errno的值定位错误了
+	              //最后真正的返回值return -1;
 }
 
 int sys_whoami(char* name, unsigned int size)
-/***
-function:将msg拷贝到name指向的用户地址空间中,确保不会对name越界访存(name的大小由size说明)
-return: 拷贝的字符数。如果size小于需要的空间,则返回“­-1”,并置errno为EINVAL。
-****/
-{ 	
-	//msg的长度大于 size
-	int len = 0;
-	for(;msg[len]!='\0';len++);
-	if(len > size)
-	{
-		return -(EINVAL);
-	}
-	int i = 0;
-	//把msg 输出至 name
-	for(i=0; i<size; i++)
-	{
-		put_fs_byte(msg[i],name+i);
-		if(msg[i] == '\0') break; //字符串结束
-	}
-	return i;
+{
+    int n=strlen(username);
+    if(n<size)
+    {
+        for(int i=0;i<n;i++)
+        {
+            put_fs_byte(username[i], &name[i]);  //关键的put_fs_byte(内核态数据,用户态的地址)
+        }
+    }
+    else
+        n=-EINVAL;
+    return n;
 }
