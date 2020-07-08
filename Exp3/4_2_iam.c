0ee6caf5d401a2f8cbcdf888df12bac9b6ed369d
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-08 13:49:13 +0800

    Create 4_2_iam.c

---
+++ Exp3/4_2_iam.c
@@ -0,0 +1,12 @@
+#include <errno.h>
+#define __LIBRARY__     /* 有它，_syscall1 等才有效。详见unistd.h */
+#include <unistd.h>     /* 有它，编译器才能获知自定义的系统调用的编号 */
+#include <stdio.h>
+
+_syscall1(int, iam, const char*, name);    /* iam()在用户空间的接口函数 */
+
+int main(int argc,char ** argv)
+{
+	iam(argv[1]);
+	return 0;
+}
