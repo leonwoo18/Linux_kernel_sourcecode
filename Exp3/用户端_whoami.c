318dbe51c1133a70c40a296edd7aa6d66410792f
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 04:06:29 +0800

    Update 用户端_whoami.c

--- Exp3/用户端_whoami.c
+++ Exp3/用户端_whoami.c
@@ -1,13 +1,11 @@
-#include <errno.h>
-#define __LIBRARY__       /* 有它，_syscall1 等才有效。详见unistd.h */
-#include <unistd.h>       /* 有它，编译器才能获知自定义的系统调用的编号 */
-
-_syscall2(int, whoami,char*,name,unsigned int,size);    /* whoami()在用户空间的接口函数 */
-
-int main()
+#include<usname.h>
+#include<stdio.h>
+int main(void)
 {
-	char s[30];
-	whoami(s,30);
-	printf("%s",s);
-	return 0;
+    char name[24];   //在用户态申请一个name数组空间
+	
+    whoami(name, 24);      //iam()会去调用封装在inputname.h中的接口函数
+	                   //传入用户态name数组地址，和size要求
+    printf("%s",name);
+    return 0;
 }
