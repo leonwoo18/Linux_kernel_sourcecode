5d2c77bd02d4493d2f8a6999f8eb14355b78ffae
leonwoo18 <55338201+leonwoo18@users.noreply.github.com>
2020-07-09 14:32:21 +0800

    Update 用户端_whoami.c

--- Exp3/用户端_whoami.c
+++ Exp3/用户端_whoami.c
@@ -6,6 +6,6 @@ int main(void)
 	
     whoami(name, 24);      /*iam()会去调用封装在inputname.h中的接口函数*/
 	                   /*传入用户态name数组地址，和size要求*/
-    printf("MY name is %s\n",name);
+    printf("%s\n",name);
     return 0;
 }
